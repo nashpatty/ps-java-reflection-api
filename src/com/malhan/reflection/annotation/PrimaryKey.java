2f8645f5eeefa82b69fb526d301a572cffcdaca3
nashpatty <m.malhan.niazi@gmail.com>
2022-07-16 10:09:00 -0400

    add annotation retention policy to the annotation

--- src/com/malhan/reflection/annotation/PrimaryKey.java
+++ src/com/malhan/reflection/annotation/PrimaryKey.java
@@ -2,5 +2,10 @@ package com.malhan.reflection.annotation;
 
 // interface defining the PrimaryKey annotation
 // special interface declaration in the form of @interface
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {
 }
