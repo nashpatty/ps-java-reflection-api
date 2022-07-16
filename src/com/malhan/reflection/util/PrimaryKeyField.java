9fea8a4e2a24a06d130af577a59b373e74759269
nashpatty <m.malhan.niazi@gmail.com>
2022-07-16 10:09:39 -0400

    add Field object type accessors, and parameters to the objects

--- src/com/malhan/reflection/util/PrimaryKeyField.java
+++ src/com/malhan/reflection/util/PrimaryKeyField.java
@@ -1,4 +1,20 @@
 package com.malhan.reflection.util;
 
+import java.lang.reflect.Field;
+
 public class PrimaryKeyField {
+
+    private Field field;
+
+    public PrimaryKeyField(Field field) {
+        this.field = field;
+    }
+
+    public String getName() {
+        return this.field.getName();
+    }
+
+    public Class<?> getType() {
+        return this.field.getType();
+    }
 }
