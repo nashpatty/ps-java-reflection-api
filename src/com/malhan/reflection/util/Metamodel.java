8d08988d738ad364389c43f6432622618e27d8de
nashpatty <m.malhan.niazi@gmail.com>
2022-07-16 10:10:38 -0400

    add methods to return the primary key and columns field metadata

--- src/com/malhan/reflection/util/Metamodel.java
+++ src/com/malhan/reflection/util/Metamodel.java
@@ -1,5 +1,10 @@
 package com.malhan.reflection.util;
 
+import com.malhan.reflection.annotation.Column;
+import com.malhan.reflection.annotation.PrimaryKey;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Metamodel<T> {
@@ -19,11 +24,28 @@ public class Metamodel<T> {
 
     // For the PrimaryKey annotation
     public PrimaryKeyField getPrimaryKey() {
-        return null;
+        Field[] fields = clss.getDeclaredFields();
+        for (Field field : fields) {
+            PrimaryKey primaryKey = field.getAnnotation(PrimaryKey.class);
+            if(primaryKey != null) {
+                PrimaryKeyField primaryKeyField = new PrimaryKeyField(field);
+                return primaryKeyField;
+            }
+        }
+        throw new IllegalArgumentException("No primary key in class " + clss.getSimpleName());
     }
 
     // For the ColumnField annotation
     public List<ColumnField> getColumns() {
-        return null;
+        Field[] fields = clss.getDeclaredFields();
+        List<ColumnField> columnFields = new ArrayList<>();
+        for (Field field : fields) {
+            Column column = field.getAnnotation(Column.class);
+            if(column != null) {
+                ColumnField columnField = new ColumnField(field);
+                columnFields.add(columnField);
+            }
+        }
+        return columnFields;
     }
 }
